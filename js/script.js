5f7945ca9ed078b38969c01cb0f4f3b7057cdcdc
zakiabdussyadad <zakiabdussyadad01@gmail.com>
2024-06-28 21:45:37 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,6 +15,43 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
+// Tambahkan event listener ke input search-box
+searchBox.addEventListener("input", filterProducts);
+
+// Fungsi untuk memfilter produk berdasarkan input
+function filterProducts() {
+  // Ambil nilai dari input search-box
+  const query = searchBox.value.toLowerCase();
+
+  // Ambil semua product-card
+  const productCards = document.querySelectorAll(".product-card");
+
+  // Tampilkan atau sembunyikan product-card berdasarkan input
+  productCards.forEach((card) => {
+    const itemName = card
+      .querySelector(".product-content h3")
+      .textContent.toLowerCase();
+    if (itemName.includes(query)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
+
+searchBox.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    filterProducts();
+  }
+});
+
+// Tambahkan event listener ke label search-box
+const searchLabel = document.querySelector("label[for='search-box']");
+searchLabel.addEventListener("click", () => {
+  searchBox.focus();
+  filterProducts();
+});
+
 // Toggle class active untuk shopping cart
 const shoppingCart = document.querySelector(".shopping-cart");
 // Ketika cart di klik
